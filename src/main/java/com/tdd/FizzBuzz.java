174ce06a4c199974cff5045ba562eb8805a434aa
来一 <tom.lai19871106@gmail.com>
2019-05-27 19:38:05 +0800

    refactor

--- src/main/java/com/tdd/FizzBuzz.java
+++ src/main/java/com/tdd/FizzBuzz.java
@@ -1,18 +1,23 @@
 package com.tdd;
 
 class FizzBuzz {
+    private static int fizzNum = 3;
+    private static int buzzNum = 5;
+    private static String fizz = "Fizz";
+    private static String buzz = "Buzz";
+
     static String of(int num) {
-        if (isFizz(num) && isBuzz(num)) return "FizzBuzz";
-        if (isFizz(num)) return "Fizz";
-        if (isBuzz(num)) return "Buzz";
+        if (isFizz(num) && isBuzz(num)) return fizz + buzz;
+        if (isFizz(num)) return fizz;
+        if (isBuzz(num)) return buzz;
         return String.valueOf(num);
     }
 
     private static boolean isBuzz(int num) {
-        return num % 5 == 0 || String.valueOf(num).contains("5");
+        return num % buzzNum == 0 || String.valueOf(num).contains(String.valueOf(buzzNum));
     }
 
     private static boolean isFizz(int num) {
-        return num % 3 == 0 || String.valueOf(num).contains("3");
+        return num % fizzNum == 0 || String.valueOf(num).contains(String.valueOf(fizzNum));
     }
 }
