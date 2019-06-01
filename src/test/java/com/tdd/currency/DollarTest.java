8c83ff379cfbc5ccdd73bb2ea0eba1bae9e3a3d9
来一 <tom.lai19871106@gmail.com>
2019-06-01 22:16:21 +0800

    完成需求： 1. 实现equal()函数

--- src/test/java/com/tdd/currency/DollarTest.java
+++ src/test/java/com/tdd/currency/DollarTest.java
@@ -14,7 +14,15 @@ class DollarTest {
     @Test
     void DollarMultiplyTest() {
         Dollar five = new Dollar(5);
-        assertThat(five.times(2)).isEqualTo(new Dollar(10));
-        assertThat(five.times(3)).isEqualTo(new Dollar(15));
+        Dollar product = five.times(2);
+        assertThat(product.amount()).isEqualTo(10);
+        product = five.times(3);
+        assertThat(product.amount()).isEqualTo(15);
+    }
+
+    @Test
+    void DollarEqualityTest(){
+        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
+        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
     }
 }
