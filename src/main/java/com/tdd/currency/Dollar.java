6e9d36effb88121fa6e53d2feae7daa7aceeeaec
来一 <tom.lai19871106@gmail.com>
2019-06-01 22:10:11 +0800

    完成需求： 1. 实现equal()函数

--- src/main/java/com/tdd/currency/Dollar.java
+++ src/main/java/com/tdd/currency/Dollar.java
@@ -1,5 +1,7 @@
 package com.tdd.currency;
 
+import java.util.Objects;
+
 /**
  * Created with IntelliJ IDEA.
  * User: lai.yi
@@ -9,12 +11,25 @@ package com.tdd.currency;
 public class Dollar {
     int amount;
 
-    public Dollar(int amount) {
+    Dollar(int amount) {
         this.amount = amount;
     }
 
-    public Dollar times(int multiplier) {
+    Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 
     public int amount() {
